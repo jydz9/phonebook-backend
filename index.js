0844d65984e37846ede79f3973676762a6644d1b
Jim Yang <76235469+jydz9@users.noreply.github.com>
2022-08-18 18:10:42 -0500

    new build

--- index.js
+++ index.js
@@ -96,8 +96,8 @@ app.post('/api/persons', (request,response) => {
     }
     
     persons = persons.concat(newPerson)
-
-    response.json(persons)
+    console.log(persons)
+    response.json(newPerson)
 })
 
 
@@ -123,7 +123,14 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running opn port ${PORT}`)
+    console.log(`Server running open port ${PORT}`)
 })
