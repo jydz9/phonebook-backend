5b203e6cc3a460516d7fac816ca95d2404e9f66c
Jim Yang <76235469+jydz9@users.noreply.github.com>
2022-08-18 18:16:16 -0500

    fix error

---
+++ build/precache-manifest.fa31f4a3383f249d773db0254f819a96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4028b2ce56efcd96ed0e",
+    "url": "/static/css/main.e70f33e4.chunk.css"
+  },
+  {
+    "revision": "4028b2ce56efcd96ed0e",
+    "url": "/static/js/main.99e6bd5c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "720096b0a7fca4f712e1",
+    "url": "/static/js/2.c941e6ef.chunk.js"
+  },
+  {
+    "revision": "7877e7dc6689fa638920d063668cd525",
+    "url": "/index.html"
+  }
+];
